67fa45c5b3757ca294013f63b7079029f5a7821d
lijinghuatongxue <woshilijinghua@gmail.com>
2020-05-30 17:48:01 +0800

    🚀：增加同步脚本demo

--- sync.sh
+++ sync.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-repourl="registry.aliyuncs.com/k8sxio"
 images=(
-kube-proxy-amd64:v1.9.0 
-kube-scheduler-amd64:v1.9.0 
-kube-controller-manager-amd64:v1.9.0 
-kube-apiserver-amd64:v1.9.0
-etcd-amd64:3.1.10 
-pause-amd64:3.0 
-kubernetes-dashboard-amd64:v1.8.3 
-k8s-dns-sidecar-amd64:1.14.7 
-k8s-dns-kube-dns-amd64:1.14.7
-k8s-dns-dnsmasq-nanny-amd64:1.14.7
+kube-proxy:v1.16.0
+kube-apiserver:v1.16.0
+kube-controller-manager:v1.16.0
+kube-scheduler:v1.16.0
 )
 for image in ${images[@]} ; do
-  docker pull $repourl/$image
-  docker tag $repourl/$image gcr.io/google_containers/$image
-  docker rmi $repouri/$image
-done
+  docker pull registry.aliyuncs.com/k8sxio/$image
+  docker tag registry.aliyuncs.com/k8sxio/$image gcr.io/google_containers/$image
+  docker rmi registry.aliyuncs.com/k8sxio/$image
+done 
